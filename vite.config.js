2aeb308c44aabe9baf54f7d69b8e67377b3de315
humo-tech <yano.wni+git@gmail.com>
2023-07-09 19:23:17 +0900

    Update vite.config.js (set root as default)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: "src",
+  //root: "src",
   //base: '/webgl-school-task/',
   build: {
     outDir: './dist',
